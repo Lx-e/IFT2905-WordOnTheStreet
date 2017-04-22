30e9f14c5b560a4827752ca0c10c98b2b48609e9
Alexis <lxe.dubois@gmail.com>
2017-04-22 17:56:54 -0400

    Correction pour la sauvegarde des favoris

--- app/src/main/java/com/example/csanchez/ift2905_wordonthestreet/SourceActivity.java
+++ app/src/main/java/com/example/csanchez/ift2905_wordonthestreet/SourceActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,28 +16,25 @@ import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 import org.json.JSONException;
-import org.json.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static com.example.csanchez.ift2905_wordonthestreet.R.id.parent;
 
-
-public class SourceActivity extends AppCompatActivity {
-    private String categories[] = {};
+public class SourceActivity extends AppCompatActivity implements View.OnClickListener{
     ListView list;
-    List<String> customSources = new ArrayList();
-    boolean checked[];
-    ContextWrapper c = new ContextWrapper(this);
+
+    List<Source> allSources = new ArrayList<Source>();
+    List<Source> favoriteSources = new ArrayList<Source>();
+
+    Map<String, Source> sourcesByName = new HashMap<String, Source>();
+    Map<View, Source> viewsToSources = new HashMap<View, Source>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,8 +44,90 @@ public class SourceActivity extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.listView_news);
 
-        SourceActivity.SourceFetcher sources = new SourceActivity.SourceFetcher();
-        sources.execute();
+        SourceActivity.SourceFetcher sourcesFetcher = new SourceActivity.SourceFetcher();
+        sourcesFetcher.execute();
+        try {
+            Source[] sourceObjects = sourcesFetcher.get();
+            int g = 0;
+
+        }
+        catch (Throwable t) {
+            int h = 9;
+        }
+    }
+
+    protected void loadFavoriteSources() {
+
+        favoriteSources = new ArrayList<Source>();
+
+        String sourcesStr = getSharedPreferences("SavedData", MODE_PRIVATE).getString("CustomSources", "Nothing");//"No name defined" is the default value.
+        Log.v("TAG", "RETRIEVED: "+sourcesStr);
+
+        if(sourcesStr == null || sourcesStr.equals("Nothing")) return;
+
+        String[] sourceNames = sourcesStr.split(",");
+
+        for (String sourceName: sourceNames) {
+            sourceName = sourceName.trim();
+            Log.v("TAG", "Parsed: " + sourceName);
+            if (sourceName.length() > 0 && sourcesByName.containsKey(sourceName) && !favoriteSources.contains(sourceName))
+                favoriteSources.add(sourcesByName.get(sourceName));
+        }
+
+    }
+
+    protected void saveFavoriteSources() {
+
+        StringBuffer sourcesBuffer = new StringBuffer();
+        if (favoriteSources.size() == 0)
+            sourcesBuffer.append("Nothing");
+        else
+            for (Source source: favoriteSources) { sourcesBuffer.append(source.name.trim()).append(","); }
+
+        SharedPreferences.Editor editor = getSharedPreferences("SavedData", MODE_PRIVATE).edit();
+        editor.remove("CustomSources");
+        editor.putString("CustomSources", sourcesBuffer.toString());
+        editor.commit();
+        Log.v("TAG", "SAVING: "+ sourcesBuffer.toString());
+    }
+
+    //Inspiré de http://stackoverflow.com/questions/14509552/uncheck-all-checbox-in-listview-in-android
+    private void clearCheckboxes(ViewGroup vg) {
+        for (int i = 0; i < vg.getChildCount(); i++) {
+            View v = vg.getChildAt(i);
+            if (v instanceof CheckBox) {
+                ((CheckBox) v).setChecked(false);
+            } else if (v instanceof ViewGroup) {
+                clearCheckboxes((ViewGroup) v);
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Source source = viewsToSources.get(v);
+        if (source == null) return;
+
+        if (favoriteSources.contains(source)) {
+            favoriteSources.remove(source);
+            Log.v("TAG", "Favorite removed: " + new Gson().toJson(source));
+        }
+        else {
+            favoriteSources.add(source);
+            Log.v("TAG", "Favorite added: " + new Gson().toJson(source));
+        }
+
+        CheckBox cb = (v instanceof CheckBox) ? null : (CheckBox)v.findViewById(R.id.checkbox);
+        if (cb != null) cb.setChecked(!cb.isChecked());
+
     }
 
     public class SourceFetcher extends AsyncTask<Object, Object, Source[]> {
@@ -61,25 +138,7 @@ public class SourceActivity extends AppCompatActivity {
             Source[] sources = new Source[0];
 
             try {
-                if(categories.length == 0){
-                    sources = NewsAPI.getSources();
-                }
-                else{
-                    sources = NewsAPI.getSources(categories);
-                }
-
-                SharedPreferences prefs = getSharedPreferences("SavedData", MODE_PRIVATE);
-                String sourcesStr = prefs.getString("CustomSources", "Nothing");//"No name defined" is the default value.
-                Log.v("TAG", "RETRIEVED: "+sourcesStr);
-                customSources = new ArrayList<String>();
-                if(!sourcesStr.equals("Nothing")) {
-                    String[] sourcesArr = sourcesStr.split(",");
-                    for (int i = 0; i < sourcesArr.length; i++) {
-                        Log.v("TAG", "Parsed: " + sourcesArr[i]);
-                        customSources.add(sourcesArr[i]);
-                    }
-                }
-
+                sources = NewsAPI.getSources();
             } catch (IOException e) {
                 e.printStackTrace();
                 Log.e("TAG", "Error in Reading: " + e.getLocalizedMessage());
@@ -88,52 +147,46 @@ public class SourceActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            checked = new boolean[sources.length];
-            for(boolean cb : checked){
-                cb=false;
+
+            for (Source source: sources) {
+                allSources.add(source);
+                sourcesByName.put(source.name, source);
             }
+
+            loadFavoriteSources();
+
+
             return sources;
         }
 
         @Override
         protected void onPostExecute(final Source[] sources) {
+
+
+
             Button saveBtn = (Button) findViewById(R.id.save_button);
             Button clearBtn = (Button) findViewById(R.id.clear_custom);
 
             clearBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    SharedPreferences.Editor editor = getSharedPreferences("SavedData", MODE_PRIVATE).edit();
-                    editor.remove("CustomSources");
-                    editor.commit();
                     clearCheckboxes(list);
-                    customSources =  new ArrayList<String>();
-                    Log.v("TAG", "REMOVING: "+checkBoxesToSourceName(sources));
+                    favoriteSources.clear();
+                    saveFavoriteSources();
                 }
             });
 
             saveBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    SharedPreferences.Editor editor = getSharedPreferences("SavedData", MODE_PRIVATE).edit();
-                    editor.remove("CustomSources");
-                    editor.putString("CustomSources", checkBoxesToSourceName(sources));
-                    editor.commit();
-                    Log.v("TAG", "SAVING: "+checkBoxesToSourceName(sources));
+                    saveFavoriteSources();
                 }
             });
 
-            for(String source : customSources) {
-                int i = getSourceIndex(sources, source);
-                if(i > -1){
-                    Log.v("TAG", "Found "+source+" at "+i);
-                    checked[i] = true;
-                }
-            }
             list.setAdapter(new BaseAdapter() {
                 @Override
                 public int getCount() {
-                    return sources.length;
+                    return allSources.size();
                 }
 
                 @Override
@@ -154,78 +207,25 @@ public class SourceActivity extends AppCompatActivity {
 
                     TextView title = (TextView) convertView.findViewById(R.id.name);
                     TextView category = (TextView) convertView.findViewById(R.id.category);
-                    CheckBox cb = (CheckBox) convertView.findViewById(R.id.checkbox);
+                    CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkbox);
 
-                    title.setText(sources[position].name);
-                    category.setText(sources[position].category);
-                    cb.setChecked(checked[position]);
+                    Source source = allSources.get(position);
+                    title.setText(source.name);
+                    category.setText(source.category);
+                    checkBox.setChecked(favoriteSources.contains(source));
+
+                    convertView.setOnClickListener(SourceActivity.this);
+                    checkBox.setOnClickListener(SourceActivity.this);
+
+                    viewsToSources.put(convertView, source);
+                    viewsToSources.put(checkBox, source);
+
+                    Log.e("TAG", "cvid: " + convertView.hashCode());
+                    Log.e("TAG", "cbid: " + checkBox.hashCode());
+
                     return convertView;
                 }
-
-
             });
-
-            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-                    CheckBox cb = (CheckBox) view.findViewById(R.id.checkbox);
-                    if (cb.isChecked()) {
-                        cb.setChecked(false);
-                        customSources.remove(sources[position].name);
-                        checked[position] = false;
-                        for(String source : customSources) {
-                            Log.v("TAG",source);
-                        }
-                    }else{
-                        cb.setChecked(true);
-                        customSources.add(sources[position].name);
-                        checked[position] = true;
-                        Log.v("TAG",(new Gson().toJson(customSources)));
-                        for(String source : customSources) {
-                            Log.v("TAG","In customSources: "+source);
-                        }
-                    }
-                }
-            });
-
         }
-    }
-
-    public String checkBoxesToSourceName(Source[] arr){
-        String str = "";
-        for(int i=0; i<checked.length; i++) {
-            if(checked[i] == true){
-                str += arr[i].id+",";
-            }
-        }
-
-        return str.length() > 0 ? str.substring(0, str.length()-1): "Nothing";
-    }
-    public int getSourceIndex(Source[] src, String id){
-        for(int i=0; i<src.length; i++){
-            if(src[i].id.equals(id)){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    //Inspiré de http://stackoverflow.com/questions/14509552/uncheck-all-checbox-in-listview-in-android
-    private void clearCheckboxes(ViewGroup vg) {
-        for (int i = 0; i < vg.getChildCount(); i++) {
-            View v = vg.getChildAt(i);
-            if (v instanceof CheckBox) {
-                ((CheckBox) v).setChecked(false);
-            } else if (v instanceof ViewGroup) {
-                clearCheckboxes((ViewGroup) v);
-            }
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
     }
 }
